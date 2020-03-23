d4fff126654a0c8b507c752acff888b093a39be6
Jarrod Doyle <ff18713@bristol.ac.uk>
2020-03-23 14:27:35 +0000

    Added sinusoidal support and tidied up k-fold code

--- cw1_ff18713.py
+++ cw1_ff18713.py
@@ -4,7 +4,7 @@ import numpy as np
 from skimage import data, io, color, transform, exposure
 from scipy import stats
 import matplotlib.pyplot as plt
-from math import inf
+import random
 
 import sys
 import utilities as utils
@@ -39,6 +39,33 @@ def chebyshev(xs, order):
         new_xs = np.column_stack((new_xs, xs**(i+1)))
     return new_xs
 
+def create_folds(xs, ys, k):
+    # Shuffle xs and ys such that they maintain their pairings
+    indices = np.arange(xs.shape[0])
+    np.random.shuffle(indices)
+    shuffled_xs = xs[indices]
+    shuffled_ys = ys[indices]
+
+    # Calculate fold size, will give an error if k is not an appropriate value for the given data set
+    fold_size = len(xs) / k
+
+    x_tests = []
+    x_trains = []
+    y_tests = []
+    y_trains = []
+
+    for i in range(k):
+        r1 = int(i*fold_size)
+        r2 = int((i+1)*fold_size)
+
+        x_tests.append(shuffled_xs[r1:r2])
+        x_trains.append(np.concatenate((shuffled_xs[:r1], shuffled_xs[r2:])))
+
+        y_tests.append(shuffled_ys[r1:r2])
+        y_trains.append(np.concatenate((shuffled_ys[:r1], shuffled_ys[r2:])))
+    
+    return x_tests, x_trains, y_tests, y_trains
+
 def main(data):
     all_xs, all_ys = data
     number_of_segments = len(all_xs) // 20
@@ -49,28 +76,47 @@ def main(data):
         xs = all_xs[i*20:(i+1)*20]
         ys = all_ys[i*20:(i+1)*20]
 
-        x_train = xs[:15]
-        x_test = xs[15:]
+        # Setup folds for cross-validation.
+        k = 10
+        x_tests, x_trains, y_tests, y_trains = create_folds(xs, ys, k)
 
-        y_train = ys[:15]
-        y_test = ys[15:]
-
-        lowest_cve = inf
+        lowest_cve = np.inf
         best_order = None
-        for j in range(1, 6):
-            coefficients = least_squares(chebyshev(x_train, j), y_train)
-            y_hat = chebyshev(x_test, j).dot(coefficients)
-            cve = calculate_error(y_test, y_hat).mean()
+        # Linear + polynomial (cubed)
+        for j in [1, 3]:
+            # K-fold cross-validation
+            cves = []
+            for l in range(k):
+                coefficients = least_squares(chebyshev(x_trains[l], j), y_trains[l])
+                y_hat = chebyshev(x_tests[l], j).dot(coefficients)
+                cves.append(calculate_error(y_tests[l], y_hat).mean())
+            cve = np.mean(cves)
             if cve < lowest_cve:
                 lowest_cve = cve
                 best_order = j
 
-        print(f"Line segment is polynomial of order {best_order}")
-        new_xs = np.linspace(xs.min(), xs.max(), 100)
-        cs = least_squares(chebyshev(xs, best_order), ys)
+        # Sinusoidal (y = asin(wx) + b)
+        cves = []
+        for l in range(k):
+            coefficients = least_squares(np.column_stack((np.ones(x_trains[l].shape), np.sin(x_trains[l]))), y_trains[l])
+            y_hat = np.column_stack((np.ones(x_tests[l].shape), np.sin(x_tests[l]))).dot(coefficients)
+            cves.append(calculate_error(y_tests[l], y_hat).mean())
+        cve = np.mean(cves)
         
-        line_segments.append((new_xs, chebyshev(new_xs, best_order).dot(cs)))
-        total_error += calculate_error(ys, chebyshev(xs, best_order).dot(cs))
+        new_xs = np.linspace(xs.min(), xs.max(), 100)
+        if cve < lowest_cve:
+            print(f"Line segment is sinusoidal")
+            cs = least_squares(np.column_stack((np.ones(xs.shape), np.sin(xs))), ys)
+            new_ys = np.column_stack((np.ones(new_xs.shape), np.sin(new_xs))).dot(cs)
+            y_hat = np.column_stack((np.ones(xs.shape), np.sin(xs))).dot(cs)
+        else:
+            print(f"Line segment is polynomial of order {best_order}")
+            cs = least_squares(chebyshev(xs, best_order), ys)
+            new_ys = chebyshev(new_xs, best_order).dot(cs)
+            y_hat = chebyshev(xs, best_order).dot(cs)
+        
+        line_segments.append((new_xs, new_ys))
+        total_error += calculate_error(ys, y_hat)
     
     return total_error, line_segments
 
